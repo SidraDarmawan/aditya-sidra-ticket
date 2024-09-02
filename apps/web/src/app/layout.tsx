e9613cc46a5a5ef227e676425a94f9aafc3ab686
Aditya Wardana <wardanaaditya49@gmail.com>
2024-09-02 19:51:10 +0700

    a

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from 'next';
-import { Plus_Jakarta_Sans } from 'next/font/google';
-import './globals.css';
+import { Plus_Jakarta_Sans } from 'next/font/google'; // font google
+import './globals.css'; // styling
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
-import { cn } from '/@lib/utils'
+import { cn } from '@/lib/utils'
 
-const fontJakarta = Plus_Jakarta_Sans({
+const fontJakarta = Plus_Jakarta_Sans({ // font yg dipakai
   subsets: ['latin'],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-sans",
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontJakarta.variable)}>
         <Header />
         {children}
         <Footer />
