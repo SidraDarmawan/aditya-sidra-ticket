960027bbc437dcec719284c3f3d90da4fb522f6a
aditwar <wardanaaditya49@gmail.com>
2024-09-05 17:17:53 +0700

    a

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -77,7 +77,7 @@ type FooterColumnProps = {
 const FooterColumn = ({ title, children }: FooterColumnProps) => {
   return (
     <div className="flex flex-col gap-5">
-      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      <h4 className="text-2xl"><b>{title}</b></h4>
       {children}
     </div>
   )
