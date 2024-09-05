7e5d848cb76eef59c657b5206049bd7f0355072e
aditwar <wardanaaditya49@gmail.com>
2024-09-05 15:49:35 +0700

    a

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,87 +1,42 @@
+import { FaEnvelope } from "react-icons/fa";
+
+
 export default function Footer() {
   return (
   <>
-    <div className="row">
-      <div className="col-lg-3 col-md-6">
-        <div className="footer-widget footer-about">
-          <div className="footer-logo">
-            <img src="https://www.quickobook.com/assets/img/logo4.png" className="img-fluid" alt="logo">
-          </div>
-          <div className="footer-about-content">
-            <p>QuickoBook is a registered start up company empaneled with 10000+ Doctors, 500+ Hospitals, lives touched of more than 2 Million patients.</p>
-            <div className="social-icon">
-              <ul>
-                <li>
-                  <a href="https://www.facebook.com/quickobook/?ref=page_internal" target="_blank"><i className="fab fa-facebook-f"></i> </a>
-                </li>
-                <li>
-                  <a href="https://x.com/Quicko_book" target="_blank"><i className="fab fa-twitter"></i> </a>
-                </li>
-                <li>
-                  <a href="https://www.linkedin.com/company/qwkpro" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com/quick.obook/" target="_blank"><i className="fab fa-instagram"></i></a>
-                </li>
-                <li>
-                  <a href="https://www.youtube.com/@quickobook2886" target="_blank"><i className="fab fa-youtube"></i> </a>
-                </li>
-              </ul>
+    <section className="bg-slate-900 md:bg-[#0C0C0C] w-full pt-32">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col mx-auto">
+          <h1 className="text-white text-center font-serif text-3xl md:text-4xl">Come Talk With Us</h1>
+          <div className="text-gray-300 text-center text-[10px] font-serif pt-14 md:flex md:justify-between md:px-20 md:text-sm">
+            <div className="flex flex-col items-center pb-16">
+              <p className="pb-3">STAY UP TO DATE <span className="block">WITH OUR NEW SLETTER</span></p>
+              <div className="flex justify-center items-center rounded-full border w-10 h-10 ">
+                <FaEnvelope />
+              </div>
             </div>
+            <div className="flex flex-col items-center pb-16">
+              <p className="pb-3">CONTACT OUR GENERAL MANAGER</p>
+              <button className="border px-3 py-2 rounded-sm">VALERIE VAN DELFT</button></div>
+                <div className="flex flex-col items-center  pb-16">
+                  <p className="pb-3">AGENCY PRESENTATION</p>
+                  <div className="flex justify-center items-center rounded-full border w-10 h-10 ">
+                  </div>
+                </div>
           </div>
         </div>
       </div>
-      <div className="col-lg-3 col-md-6">
-        <div className="footer-widget footer-menu">
-          <h2 className="footer-title">For Information</h2>
-          <ul>
-            <li><a href="https://www.quickobook.com/about">About Us</a></li>
-            <li><a href="https://youtu.be/4abdBRWh6k0">Booking Guide</a></li>
-            <li><a href="https://pharmacy.quickobook.com/">Pharmacy</a></li>
-            <li><a href="https://www.quickobook.com/careers">Careers</a></li>
-            <li><a href="https://www.quickobook.com/press_release">Press Release</a></li>
-            <li><a href="https://www.quickobook.com/faq">FAQ'S</a></li>
-          </ul>
+      <div className="w-full  bg-gray-800  py-5 md:bg-[#131313]">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row md:justify-around md:gap-5 md:px-10 items-center text-gray-300 text-sm font-semibold">
+          <p className="hidden md:flex">CONTACT US</p>
+          <p className="">NEWSLETTER</p>
+          <p className="hidden md:flex">PODCAST</p>
+          <p className="hidden md:flex">1 SITE PAR JOUR</p>
+          <p className="hidden md:flex">AGENCY PRESENTATION</p>
+          <p className="md:hidden">AGENCY PRES.</p>
         </div>
       </div>
-      <div className="col-lg-3 col-md-6">
-        <div className="footer-widget footer-menu">
-          <h2 className="footer-title">Helpful Links</h2>
-          <ul>
-            <li><a href="https://www.quickobook.com/search">Book Appointment</a></li>
-            <li><a href="https://www.quickobook.com/search">Search for doctors</a></li>
-            <li><a href="https://www.quickobook.com/hospitals">Search for hospitals </a></li>
-            <li><a href="https://www.quickobook.com/book-test">Book Lab/Diagnostics Test</a></li>
-            <li><a href="https://www.quickobook.com/franchisee">Franchisee Register</a></li>
-            <li><a href="https://www.quickobook.com/">SMS Booking</a></li>
-            <li><a href="https://www.quickobook.com/">Services</a></li>
-          </ul>
-        </div>
-      </div>
-      <div className="col-lg-3 col-md-6">
-        <div className="footer-widget footer-contact">
-          <h2 className="footer-title">Contact Us</h2>
-          <div className="footer-contact-info">
-            <div className="footer-address">
-              <span><i className="fas fa-map-marker-alt"></i></span>
-              <p> QWKPRO CONSULTANCY PVT LTD,<br> Silchar, Assam </p>
-            </div>
-            <p>
-              <i className="fas fa-phone-alt"></i>
-              +91 943 520 0024
-            </p>
-            <p className="mb-0">
-              Customer Support :<br>
-              <i className="fas fa-envelope"></i>
-              support@quickobook.com<br><br>
-              Official Queries :<br>
-              <i className="fas fa-envelope"></i>
-              info@quickobook.com
-            </p>
-          </div>
-        </div>
-      </div>
-  </div>    
+    </section>
   </>
 );
 }
