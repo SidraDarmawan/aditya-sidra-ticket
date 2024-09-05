f5a4c865d0a122caa99a796560a0e76253117e5c
Aditya Wardana <wardanaaditya49@gmail.com>
2024-09-06 03:21:30 +0700

    a

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -18,9 +18,9 @@ const Footer = () => {
           </Link>
 
           <div className='flex flex-wrap gap-10 sm:justify-between md:flex-1'>
-            {FOOTER_LINKS.map((columns) => (
+          {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
-                <ul className="flex flex-col gap-4 text-gray-30 min-w-[196px]">
+                <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
                     <Link href="/" key={link}>
                       {link}
