9f9609b07d8ebed8368041b03f35288b5b6de2ad
Aditya Wardana <wardanaaditya49@gmail.com>
2024-09-11 04:36:37 +0700

    a

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,11 +1,15 @@
+import LoginForm from "@/components/auth/login-form";
 
 
 export default function Login () {
-    
-    
+  
   return (
     <>
-    
+    <section className="w-full pt-[112px] my-[50px]">
+      <div className=" flex items-center justify-center">
+        <LoginForm />
+      </div>
+    </section>
     </>
   );
 };
--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,10 +1,15 @@
+import RegisterForm from "@/components/auth/register-form";
 
 
 export default function Register () {
   
   return (
     <>
-    
+    <section className="w-full pt-[112px] my-[50px]">
+      <div className=" flex items-center justify-center">
+        <RegisterForm />
+      </div>
+    </section>
     </>
   );
 };
