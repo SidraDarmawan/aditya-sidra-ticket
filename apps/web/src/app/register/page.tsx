688ed6a3427183252302e410a200e495642fc54b
aditwar <wardanaaditya49@gmail.com>
2024-09-10 12:59:03 +0700

    a

--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -1,43 +1,10 @@
-import Image from 'next/image'
-import Link from "next/link";
-import { PasskeyModal } from "@/components/PasskeyModal";
-import { RegForm } from '@/components/forms/RegForm';
 
-export default function Register ({ searchParams }: SearchParamProps) {
-  const isAdmin = searchParams?.admin === "true";
-    
+
+export default function Register () {
+  
   return (
     <>
-    <div className="flex pt-[112px] h-[650px]">
-
-      {/* //! OTP VERIFICATION PASSKEY MODEL */}
-      {isAdmin && <PasskeyModal />}
-
-      <section className="remove-scrollbar container">
-        <div className="pt-[30px] items-center justify-center">
-          
-
-          <RegForm />
-
-          <div className="text-14-regular mt-[25px] flex justify-between">
-            <p className="justify-items-end text-white xl:text-left">
-              © 2025 AditSidra Fest
-            </p>
-            <Link href="/login" className="text-[#ff4693]">
-              Already have an Account?
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      <Image
-        src="/assets/images/onboarding-img.png"
-        height={1000}
-        width={1000}
-        alt="Concert"
-        className="side-img max-w-[50%]"
-      />
-      </div>
+    
     </>
   );
 };
