99d2b5c8c6a613192e3013e161243db7a19faf7a
Aditya Wardana <wardanaaditya49@gmail.com>
2024-09-12 00:52:19 +0700

    a

--- apps/api/src/controllers/author.controller.ts
+++ apps/api/src/controllers/author.controller.ts
@@ -1,20 +1,36 @@
 import { Request, Response } from "express";
 import prisma from "../prisma";
+import { genSalt, hash } from "bcrypt";
 
 export class AuthorController {
     async createAuthor(req: Request, res: Response) {
         try {
+            // nerima db dari req.body
             const { name, email, password } = req.body
-            const author = await prisma.author.create({
-                data: { name, email, password }
+
+            // dapetin secara UNIQUE, yg mana email itu didapat dr req.body
+            const existingAuthor = await prisma.author.findUnique({
+                where: { email: email} // ini pengecekan
             })
-            res.status(201).send({
+            // ini untuk validasi, jika sudah ada Author nya (existingAuthor), throw ini akan ditangkap oleh catch err
+            if(existingAuthor) throw "email has been used"
+
+            // ini jika tidak, maka menjalankan ENSCRYPT dari password
+            const salt = await genSalt(10);
+            const hashPassword = await hash(password, salt); // password yg sudah ditangkap di hash dulu
+            
+            
+            // ini nge Create
+            const author = await prisma.author.create({
+                data: { name, email, password: hashPassword } // password: hashPassword nya ditambahkan jadi langsung dibuatkan dalam db
+            })
+            res.status(201).send({  // ini hanya pemberitahuan tidak err
                 status: 'ok',
                 msg: 'author created!',
                 author
             })
         } catch (err) {
-            res.status(400).send({
+            res.status(400).send({ // akan ditanggkap ini
                 status: 'error',
                 msg: err
             })
